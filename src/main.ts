333361d9cf5ad2ba6fc13992f796c713f9a5cb0f
Adam Suskin <suskin@joincandidhealth.com>
2022-09-10 21:08:09 -0600

    Install packages and test check creation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,48 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+
+
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { Octokit } from '@octokit/rest';
+
+const { GITHUB_TOKEN } = process.env;
+
+interface Annotation {
+  path: string;
+  start_line: number;
+  end_line: number;
+  start_column: number;
+  end_column: number;
+  annotation_level: "failure";
+  message: string;
+}
+
+async function createCheck(check_name: string, title: string, annotations: Annotation[]) {
+  const octokit = new Octokit({ auth: GITHUB_TOKEN });
+  const res = await octokit.checks.listForRef({
+    check_name,
+    ...github.context.repo,
+    ref: github.context.sha
+  });
+
+  const check_run_id = res.data.check_runs[0].id;
+
+  await octokit.checks.update({
+    ...github.context.repo,
+    check_run_id,
+    output: {
+      title,
+      summary: `${annotations.length} errors(s) found`,
+      annotations
+    }
+  });
+}
+
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
     core.setOutput('time', new Date().toTimeString())
+    await createCheck("test-check-name", "test-check-name", [{ path: "README.md", start_line: 1, end_line: 1, start_column: 1, end_column: 2, annotation_level: "failure", message: "Test check failure" }])
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
