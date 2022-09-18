6d1a8e5d79c120deee3501f5c1903a06c424a910
Adam Suskin <suskin@joincandidhealth.com>
2022-09-18 15:32:09 -0400

    use input yaml file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Regex Lint Github Action (regex-lint-gh-action)
+
+### For Demonstration Purposes
+
+Hello! This should cause an error as it contains the word hello which violates the test rule. See the last PR as an example of how the annotations appear.
--- src/main.ts
+++ src/main.ts
@@ -1,93 +1,36 @@
-
-
 import * as core from '@actions/core';
-import * as github from '@actions/github';
-import { Octokit } from '@octokit/rest';
-
-const { GITHUB_TOKEN } = process.env;
-
+import { promises as fs } from 'fs';
+import * as yaml from 'js-yaml';
 interface Annotation {
-  path: string;
-  start_line: number;
-  end_line: number;
-  start_column: number;
-  end_column: number;
-  annotation_level: "failure";
+  title: string;
+  file: string;
+  startLine: number;
+  endLine: number;
+  startColumn: number;
+  endColumn: number;
   message: string;
 }
 
-async function createCheck(title: string, annotations: Annotation[]) {
-  const octokit = new Octokit({ auth: GITHUB_TOKEN });
-  const res = await octokit.checks.listForRef({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    ref: github.context.payload.pull_request?.head.sha
-  });
-  core.info("github context")
-  core.info(JSON.stringify(github.context))
-  core.info("res");
-  core.info(JSON.stringify(res));
+async function loadConfig() {
+  const file = core.getInput('file');
+  const content = await fs.readFile(file, 'utf8');
 
-  const check_run = res.data.check_runs.find(c => c.name === github.context.job)
-  if (check_run == null) {
-    core.info("creating new check");
-    const create_resp = await octokit.checks.create({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      head_sha: github.context.payload.pull_request?.head.sha,
-      name: github.context.job,
-      output: {
-        title,
-        summary: `${annotations.length} errors(s) found`,
-        text: "Please fix this",
-        annotations
-      },
-      status: "completed",
-      conclusion: "failure"
-    });
-    core.info(JSON.stringify(create_resp));
-  } else {
-    core.info("updating existing check");
-    const update_resp = await octokit.checks.update({
-      check_run_id: check_run.id,
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      head_sha: github.context.sha,
-      name: github.context.job,
-      output: {
-        title: title,
-        summary: `${annotations.length} errors(s) found`,
-        text: "Please fix this",
-        annotations
-      },
-      status: "completed",
-      conclusion: "failure"
-    })
-    core.info(JSON.stringify(update_resp))
-  }
+  return yaml.load(content);
 }
 
-// async function statusFail() {
-//   const octokit = new Octokit({ auth: GITHUB_TOKEN });
-//   await octokit.sta
-// }
-
-
 async function run(): Promise<void> {
-  core.info("Running action...");
-  const annotations = [{ title: "other title", path: "README.md", start_line: 1, end_line: 1, start_column: 1, end_column: 2, annotation_level: "failure", message: "Fix this line" }]
   try {
-    await createCheck("test-check-name", annotations as Annotation[])
-    core.warning("Here is a different annotation", { title: "annotation title", file: "README.md", startLine: 1, endLine: 1, startColumn: 1, endColumn: 2 })
+    const config = await loadConfig();
+    if (config == null) {
+      core.setFailed('Error in reading the input yaml file');
+      return;
+    }
+
+    core.info(JSON.stringify(config));
   } catch (error) {
     if (error instanceof Error) {
       core.warning("There was an error in run");
-      core.setOutput("test-check-name", {
-        summary: `${annotations.length} errors(s) found`,
-        text: "Please fix this",
-        annotations
-      })
-      core.setFailed(error.message)
+      core.setFailed(error.message);
     }
   }
 }
